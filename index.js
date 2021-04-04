7444c2c28f9fe1c652431336fbbfbd7e77b425cf
sandhya <sandhya.beec.16@acharya.ac.in>
2021-04-04 11:29:57 +0530

    commit

---
+++ index.js
@@ -0,0 +1,20 @@
+a();
+b();
+//  function statement 
+function a() {
+    console.log(" a called");
+}
+
+//function expression
+var b = function () {
+    console.log("b called");
+}
+ //anomyouses function
+ // used as a value
+function() {
+     
+}
+//xyz(); error
+
+//first class function
+//we can call function inside a function as a argument in parameter
